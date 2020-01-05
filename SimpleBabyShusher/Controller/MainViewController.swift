e58e9f2f7ebc7ea2c5bc0bf4f0ea0aa8a0438445
Jeremy Evans <jeremy@chiefgeek.io>
2020-01-04 19:08:00 -0500

    Upgraded branch to Swift 5

--- SimpleBabyShusher/Controller/MainViewController.swift
+++ SimpleBabyShusher/Controller/MainViewController.swift
@@ -67,9 +67,7 @@ class MainViewController: UIViewController {
         // Anything that can be thrown goes here
         do {
             // Play in background
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, with: .mixWithOthers)
-            try AVAudioSession.sharedInstance().setActive(true)
-            // Play in background end
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback, options: AVAudioSession.CategoryOptions.mixWithOthers)            // Play in background end
         } catch {
             print("Something went wrong in ViewDidLoad")
         }
@@ -100,7 +98,7 @@ class MainViewController: UIViewController {
         
         // Observers
         // This observer runs everytime the app returns to forground
-        NotificationCenter.default.addObserver(self, selector: #selector(appReturnsToForground), name: NSNotification.Name.UIApplicationWillEnterForeground, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(appReturnsToForground), name: UIApplication.willEnterForegroundNotification, object: nil)
     }
 
     func setLayout() {
@@ -197,4 +195,9 @@ class MainViewController: UIViewController {
         }
         // pulsate end
     }
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromAVAudioSessionCategory(_ input: AVAudioSession.Category) -> String {
+	return input.rawValue
 }
